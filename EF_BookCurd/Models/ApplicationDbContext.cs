1bdc5560568c5651d01a1e92553340aae5c47693
anjali buddhe <anjalibuddhe124@gmail.com>
2023-09-09 13:43:55 +0530

    Add project files.

---
+++ EF_BookCurd/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace EF_BookCurd.Models
+
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
